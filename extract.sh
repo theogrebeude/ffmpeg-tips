ac186a6dca0b8cc420433e2e3e6b436f4249cece
Théo Grébeude <theogrebeude@gmail.com>
2017-06-22 14:58:58 +0200

    Create extract.sh

---
+++ extract.sh
@@ -0,0 +1,8 @@
+// extract part of video with FFMPEG
+// 4 parameters in input 
+// firt is the input video
+// second is the start time like HH:MM:SS or like MM:SS.mm
+// third is the duration like the start time
+// fourth is the name off the output video 
+
+ffmpeg -i $1 -ss $2 -t $3 vc copcy av copy $4 
