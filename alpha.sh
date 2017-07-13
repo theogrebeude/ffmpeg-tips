271cc953546366e413bbb793234a61d75b58a81d
Théo Grébeude <theogrebeude@gmail.com>
2017-07-13 17:37:15 +0200

    correction

--- alpha.sh
+++ alpha.sh
@@ -3,4 +3,4 @@
 //juste alpha
 ffplay -i fichier.mp4 -vf "alphaextract"
 //video & alpha side by side 
-ffmplay -i alpha.mov  -vf "split=2[a][b];[b]alphaextract[alpha];[a][alpha]hstack=2"
+ffplay -i alpha.mov  -vf "split=2[a][b];[b]alphaextract[alpha];[a][alpha]hstack=2"
