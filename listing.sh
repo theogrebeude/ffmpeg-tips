252afe57e3595ca7b973c8712032dfdb41f7681f
Théo Grébeude <theogrebeude@gmail.com>
2017-11-07 11:05:48 +0100

    use function not .sh recursive

--- listing.sh
+++ listing.sh
@@ -1,40 +1,48 @@
 #!/bin/bash
-localpath="$(pwd)"
-space=""
-reg=".*\/([^\/]+)"
-if [  "$1" ]
-then
-	localpath=$1
-fi
-if [ "$2" ]
-then
-	space=$space$2
-fi
-if [ "$3" ]
-then
-	if [[ $localpath =~ $reg ]]; then debut="┣━ "${BASH_REMATCH[1]}; fi
-else
-	if [[ $localpath =~ $reg ]]; then debut="┗━ "${BASH_REMATCH[1]}; fi
-fi
-echo "$space $debut($(du -hs $localpath| cut -f1))::$(find $localpath -maxdepth 1 -type f |sort | uniq -c| wc -l) files" 
-subdircount=`find $localpath -maxdepth 1 -type d | wc -l`
-if [ $subdircount -ne 1 ]
-then
-	count="$((subdircount-1))"
-    	#echo "contient $count  sous dossier"
-	subdir=(`find $localpath -maxdepth 1 -type d |sort `)
-	for i in "${subdir[@]}"
-	do : 
-		if [ $i != $localpath ]
-		then
-			count=$((count-1))
-			if [ $count != 0 ] 
+process (){
+	localpath="$(pwd)"
+	local space=""
+	reg=".*\/([^\/]+)"
+	if [ "$1" ]
+	then
+		localpath=$1
+	fi
+	if [ "$2" ]
+	then
+		space=$space$2
+	fi
+	if [ "$3" ]
+	then
+		if [[ $localpath =~ $reg ]]; then debut="┣━ "${BASH_REMATCH[1]}; fi
+	else
+		if [[ $localpath =~ $reg ]]; then debut="┗━ "${BASH_REMATCH[1]}; fi
+	fi
+	echo "$space $debut($(du -hs $localpath| cut -f1))::$(find $localpath -maxdepth 1 -type f |sort | uniq -c| wc -l) files"
+	subdircount=`find $localpath -maxdepth 1 -type d | wc -l`
+	if [ $subdircount -ne 1 ]
+	then
+		count="$((subdircount-1))"
+			#echo "contient $count sous dossier"
+		subdir=(`find $localpath -maxdepth 1 -type d |sort `)
+		for i in "${subdir[@]}"
+		do :
+			if [ $i != $localpath ]
 			then
-				sh list.sh $i "$space  " 1
-			else
-				sh list.sh $i "$space  "
+				count=$((count-1))
+				if [ $count != 0 ]
+				then
+					process $i "$space " 1
+				else
+					process $i "$space "
+				fi
+				#echo "-----------"
 			fi
-			#echo "-----------"
-		fi
-	done	
+		done
+	fi
+}
+if [ "$1" ]
+then
+process $1
+else
+process
 fi
