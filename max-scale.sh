c2ea30980e223a0a113faca031d2d9315b900b6d
Théo Grébeude <theogrebeude@gmail.com>
2018-04-25 11:31:11 +0200

    Update max-scale.sh

--- max-scale.sh
+++ max-scale.sh
@@ -1,6 +1,6 @@
 // convert image to JPG with scale limitation 2500x2500 
 // 2 parameters in input 
-// firt is the input video
+// first is the input video
 // second  is the name off the output video 
 
 ffmpeg -i $1 -q:v 1 -filter_complex "scale='if(gte(iw\,ih),if(gte(iw\,2500),2500\,iw),if(gte(ih\,2500),iw*2500/ih\,iw))':'if(gte(iw\,ih),if(gte(iw\,2500),ih*2550/iw\,ih),if(gte(ih\,2500),2500\,ih))'" -y $2
